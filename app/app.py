55dccd1a92e9364b543eabd661f97cdb03bc73fe
Giovanna Brito <giovanna.bsilva612@gmail.com>
2025-04-06 23:36:49 -0300

    arquivos docker adicionados

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 app.register_blueprint(aluno_bp)  
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 
 # class Professor:
