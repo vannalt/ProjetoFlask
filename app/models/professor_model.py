05701b565d8c3d8107b0cc143a44db4f8a6a026c
Giovanna Brito <giovanna.bsilva612@gmail.com>
2025-04-08 22:10:13 -0300

    Atualizado pelo Spilla

---
+++ app/models/professor_model.py
@@ -0,0 +1,9 @@
+class Professor:
+     def __init__(self, id, nome, disciplina, idade, observacoes):
+         self.id = id
+         self.nome = nome
+         self.disciplina = disciplina
+         self.idade = idade
+         self.observacoes = observacoes
+         
+    
