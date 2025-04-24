ee04e05ad81ac11799a27b13893ec8911d8b2e10
Gabriella <gabriella.morais0506@gmail.com>
2025-04-23 21:17:49 -0300

    Update aluno_namespace.py

--- namespaces/aluno_namespace.py
+++ namespaces/aluno_namespace.py
@@ -1,4 +1,5 @@
 from flask_restx import Namespace, Resource, fields
 
+from models.aluno_model import listar_alunos, adicionar_aluno, aluno_por_id, atualizar_aluno, excluir_aluno
 
 api = Namespace('Pessoa',description="Operações relacionadas aos alunos")
