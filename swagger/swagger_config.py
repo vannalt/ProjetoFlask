d29013577d66faf62b0d3c7b95006b453fb1cfcf
Gabriella <gabriella.morais0506@gmail.com>
2025-05-01 18:31:26 -0300

    Update swagger_config.py

--- swagger/swagger_config.py
+++ swagger/swagger_config.py
@@ -1,1 +1,11 @@
+from swagger import api
+from swagger.namespaces.aluno_namespace import api_alunos
+from swagger.namespaces.professor_namespace import professores_ns
+from swagger.namespaces.turma_namespace import turmas_ns
 
+def configure_swagger(app):
+    api.init_app(app)
+    api.add_namespace(api_alunos, path="/alunos")
+    api.add_namespace(professores_ns, path="/professores")
+    api.add_namespace(turmas_ns, path="/turmas")
+    api.mask_swagger = False
