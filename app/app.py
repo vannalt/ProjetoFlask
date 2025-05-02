7103eb23aded7e35337c7ba3bcb4ad944b7b0e04
spillapep <spillagustavo@gmail.com>
2025-05-02 11:21:33 -0300

    atualização de banco de dados

--- app/app.py
+++ app/app.py
@@ -1,23 +1,26 @@
 from swagger.swagger_config import configure_swagger
 import pytest
-import sys
 import os
-*from config import app, db*
-from alunos.routealunos import alunos_bp
-from professores.routeprofessores import professores_bp
-from turmas.routeturmas import turmas_bp
+import sys
+from config import app, db
+from alunos.alunos_routes import alunos_bp
+from turma.turma_routes import turmas_bp
+from professor.professor_routes import professores_bp
 
 
-app = Flask(__name__)
-app.register_blueprint(aluno_bp) 
-app.register_blueprint(turma_bp) 
-app.register_blueprint(professor_bp)
+app.register_blueprint(alunos_bp, url_prefix='/api')
+app.register_blueprint(turmas_bp, url_prefix='/api')
+app.register_blueprint(professores_bp, url_prefix='/api')
 
 configure_swagger(app)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+with app.app_context():
+    db.create_all()
 
+
+
+if __name__ == '__main__':  
+    app.run(host=app.config["HOST"], port=app.config['PORT'], debug=app.config['DEBUG'])
 
 
 
--- app/models/aluno_model.py
+++ app/models/aluno_model.py
@@ -1,14 +1,38 @@
-class Aluno:
-    def __init__(self, nome, data_de_nascimento, turma_id, id=None):
-        self.id = id
-        self.nome = nome
-        self.turma_id = turma_id
-        self.data_de_nascimento = data_de_nascimento
 
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "nome": self.nome,
-            "data_de_nascimento": self.data_de_nascimento,
-            "turma_id": self.turma_id
-        }
+from turma.turma_model import Turma
+from datetime import datetime, date
+from config import db
+
+
+class Aluno(db.Model):
+  __tablename__ = "alunos"
+
+   
+  id = db.Column(db.Integer, primary_key=True)
+  nome = db.Column(db.String(100), nullable=False)
+  idade = db.Column(db.Integer, nullable=False)
+  data_nascimento = db.Column(db.Date, nullable=False)
+  nota_primeiro_semestre = db.Column(db.Float, nullable=False)
+  nota_segundo_semestre = db.Column(db.Float, nullable=False)
+  media_final = db.Column(db.Float, nullable=False)
+  
+  turma = db.relationship("Turma", back_populates="alunos")
+  turma_id = db.Column(db.Integer, db.ForeignKey("turmas.id"), nullable=False)
+
+  def __init__(self, nome, data_nascimento, nota_primeiro_semestre, nota_segundo_semestre, turma_id,media_final):
+    self.nome = nome
+    self.data_nascimento = data_nascimento
+    self.nota_primeiro_semestre = nota_primeiro_semestre
+    self.nota_segundo_semestre = nota_segundo_semestre
+    self.turma_id = turma_id
+    self.media_final = media_final
+    self.idade = self.calcular_idade()
+        
+
+
+
+
+
+   
+
+
